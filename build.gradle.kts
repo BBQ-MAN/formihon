dc821ea1060ae617f24c65925f27c8716a54cbea
BBQMAN <bbqman@gmail.com>
2026-08-04 17:58:23 +0900

    Restore extensions-lib to v16 and add buildscript classpath for Kotlin 2.4.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
+    }
+}
+
 plugins {
     id("com.android.application") version "9.3.1"
 }
@@ -33,7 +39,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     
     // Core Tachiyomi API stubs
-    compileOnly("com.github.keiyoushi:extensions-lib:18a8e26be2")
+    compileOnly("com.github.keiyoushi:extensions-lib:6e0c96cea8")
     compileOnly("com.github.null2264.injekt:injekt-core:4135455a2a")
 
     // RxJava & Coroutines
